5d8384633b47aff8a898d0cb0e2cc83336c46559
Simone <simone.esposito1986@gmail.com>
2024-01-01 18:58:00 +0100

    move file in other path

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
+
+
+# metto l'ip dell'interfaccia pubblica nel file delle variabili
+# se il server ha più interfacce, modificare questa linea di codice
 LOCAL_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | grep -v ^$)
-cat environment > .env
-echo "" >> .env
-echo "LOCAL_IP=${LOCAL_IP}" >> .env
+#LOCAL_IP=override
+sed -i "s/LOCAL_IP=.*/LOCAL_IP=${LOCAL_IP}/g" variables 
 
+
+#docker-compose -f hsh.yaml --env-file variables down
 
 printf "Prepare certs for kibana ";
-openssl req -x509 -newkey rsa:4096 -keyout ./kibana/kibana.key -out ./kibana/kibana.crt -sha256 -days 3650 -nodes -subj "/C=IT/ST=Italy/L=Naples/O=HSH/OU=HSH-IT/CN=${LOCAL_IP}" 2>/dev/null
+openssl req -x509 -newkey rsa:4096 -keyout kibana.key -out kibana.crt -sha256 -days 3650 -nodes -subj "/C=IT/ST=Italy/L=Naples/O=HSH/OU=HSH-IT/CN=${LOCAL_IP}" 2>/dev/null
 printf "\n";
 
-docker-compose -f search-compose.yaml down 
-docker-compose -f search-compose.yaml up -d --build elastic_master kibana
-
+docker-compose -f hsh.yaml -p hsh --env-file variables down --rmi local
+docker-compose -f hsh.yaml -p hsh --env-file variables up -d --build elastic_master kibana
 
 OK_ELASTIC=2
 printf "Provo a generare la password dell'utente elastic ";
@@ -21,16 +25,14 @@ while [ ${OK_ELASTIC} != 0 ] ; do
 	OK_ELASTIC=$?
 	printf ".";
 done;
-
-printf " elastic / ${ELASTIC_PASSWORD}\n";
-
-echo "ELASTIC_PASSWORD=${ELASTIC_PASSWORD}" >> .env
+sed -i "s/ELASTIC_PASSWORD=.*/ELASTIC_PASSWORD=${ELASTIC_PASSWORD}/g" variables 
+printf "\n";
 
 printf "Create enrollment token for kibana .. ";
 
 KIBANA_ENROLLMENT=$(docker exec -it elastic_master /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana 2>/dev/null)
 
-echo "KIBANA_ENROLLMENT=${KIBANA_ENROLLMENT}" >> .env
+sed -i "s/KIBANA_ENROLLMENT=.*/KIBANA_ENROLLMENT=${KIBANA_ENROLLMENT}/g" variables 
 
 printf "${KIBANA_ENROLLMENT}\n";
 printf "Auto setting enrollment token on kibana .. "
@@ -39,52 +41,45 @@ printf "ok\n";
 printf "Restart kibana\n";
 docker restart kibana >/dev/null
 
+
 printf "Create a enrollment token for new nodes to joins .. "
 
 ELASTIC_NODE_ENROLL=$(docker exec -it elastic_master /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node 2>/dev/null)
 
-echo "ELASTIC_NODE_ENROLL=${ELASTIC_NODE_ENROLL}" >> .env
-
-printf "${ELASTIC_NODE_ENROLL}\n";
-
+sed -i "s/ELASTIC_NODE_ENROLL=.*/ELASTIC_NODE_ENROLL=${ELASTIC_NODE_ENROLL}/g" variables 
 printf "Starting elastic cold instance and join to cluster\n";
-docker-compose -f search-compose.yaml up -d --build elastic_cold
+docker-compose -f hsh.yaml -p hsh --env-file variables up -d --build elastic_cold
+
+
+
+_IP=$(grep -oP 'IP_ADDRESS_ELASTIC_DATA=\K([0-9\.])+' variables)
+
+printf "Create ILM policy\n";
+#AUTH=$(echo -n "elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]}" | base64)
+#curl -k https://10.10.84.10:9200/_ilm/policy/hsh_policy -XPUT -H "Content-Type: application/json" \
+#-H "Authorization: Basic ${AUTH}" --data-binary @hsh_policy.json
+
+curl -k https://${_IP}:9200/_ilm/policy/hsh_policy -XPUT -H "Content-Type: application/json" \
+-u elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]} --data-binary @elastic/hsh_policy.json
+
+curl -k https://${_IP}:9200/_component_template/hsh_component -XPUT -H "Content-Type: application/json" \
+-u elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]} --data-binary @elastic/hsh_component.json
+
+curl -k https://${_IP}:9200/_index_template/power_consume -XPUT -H "Content-Type: application/json" \
+-u elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]} --data-binary @elastic/hsh_power.json
+
+curl -k https://${_IP}:9200/_data_stream/hsh_metrics_power -XPUT -H "Content-Type: application/json" \
+-u elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]}
+
+curl -k https://${_IP}:9200/_data_stream/hsh_metrics_log_fe -XPUT -H "Content-Type: application/json" \
+-u elastic:${ELASTIC_PASSWORD%%[[:cntrl:]]}
+
+docker-compose -f hsh.yaml -p hsh --env-file variables up -d --build npm homer bitwarden
 
 
 
 
-. ./.env
-
-printf "#################### INFO ####################\n";
-printf "Elastic Master Node URL\t: https://${LOCAL_IP}:${EXPOSE_PORT_ELASTIC_DATA} or https://${IP_ADDRESS_ELASTIC_DATA}:9200\n";
-printf "Elastic Cold Node URL\t: https://${LOCAL_IP}:${EXPOSE_PORT_ELASTIC_COLD} or https://${IP_ADDRESS_ELASTIC_COLD}:9200\n";
-printf "Kibana and Elastic User\t: elastic\n";
-printf "Kibana and Elastic PWD\t: ${ELASTIC_PASSWORD}\n";
-printf "Kibana URL\t\t: http://${LOCAL_IP}:${EXPOSE_PORT_KIBANA} or http://${IP_ADDRESS_KIBANA}:5601\n";
-printf "Enrollment token for kibana\t: ${KIBANA_ENROLLMENT}\n";
-printf "Enrollment token for Cold node\t: ${ELASTIC_NODE_ENROLL}\n";
-
-
-
-
-
-# docker exec -it elastic_master elasticsearch-create-enrollment-token -s node
-# docker exec -it elastic_cold elasticsearch --enrollment-token eyJ2ZXIiOiI4LjEwLjIiLCJhZHIiOlsiMTAuMTAuODAuMjE2OjkyMDAiXSwiZmdyIjoiMGNmODUwYTFhYTBhY2EzNzUxYTNlMjI4OWZiMDZmZTAwY2I1OGYzYTliNWE5ZTdhNTc4ZjdmMjE0YjdhODFkNSIsImtleSI6ImxDWEdob3NCcnkzd1VPQW5RdFR4OmNWZ0FYMFJtUzBHVFN6U0N4UTBKX2cifQ==
-
-# echo discovery.seed_hosts: >> elasticsearch.yml
-# echo "   - 10.10.80.217:9201"
-#   - 192.168.1.11 
-#   - seeds.mydomain.com 
-#   - [0:0:0:0:0:ffff:c0a8:10c]:9301
-# discovery.zen.ping.unicast.hosts: ["10.10.80.216", "10.10.80.217"]
-
-
-
-
-
-
-
-
-
-
+printf "\nKibana console: https://${LOCAL_IP}:5601\n";
+printf "username: elastic\n";
+printf "password: ${ELASTIC_PASSWORD}\n";
 
